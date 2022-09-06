64cb4d4b7f88925646c098d2e27eae90045f3f75
Christian Rivero Arnez <70019894+DwijanX@users.noreply.github.com>
2022-09-06 09:49:15 -0400

    added alert when users todo title is too long

--- src/PendientesManager.js
+++ src/PendientesManager.js
@@ -4,11 +4,19 @@ function showEmptyTitleAlert()
 {
     alert("No es posible registrar una tarea pendiente sin descripción")
 }
+function showThatPendienteIsTooLong()
+{
+    alert("No es posible registrar una tarea con una descripcion de mas de 50 caracteres")
+}
 function addPendiente(pendienteTitle)
 {
     if(pendienteTitle=="" || pendienteTitle==" " )
     {
         showEmptyTitleAlert()
+    }
+    else if (pendienteTitle.length>50)
+    {
+        showThatPendienteIsTooLong()
     }
     else
     {
